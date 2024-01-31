26e2714becf3abf67512debd7352a584be8b85e7
Pramodh <112399730+RPramodh@users.noreply.github.com>
2024-01-31 09:48:58 +0000

    The second commit

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+
+# TASK -1 - 1st Play with this, Flask based environment, then go for, implementing the ML model
 # from flask import Flask, render_template, request
 
 # app=Flask(__name__)
 
-# @app.route("/",methods=['GET', 'POST'])
+# @app.route("/",methods=['GET', 'POST'])   # for fecthing
 # def home():
 #     text = ""
-#     if request.method == 'POST':
+#     if request.method == 'POST':     # if any text or parameter to post method
 #         text = request.form.get('email-content')
 #     return render_template("index.html",text=text)
 
@@ -14,13 +16,13 @@
 # if __name__=="__main__":
 #     app.run(debug=True)
 
-
+# Task -2 - This is were, it starts, how to implement the ML model
 from flask import Flask, render_template, request
 import pickle
 
 app = Flask(__name__)
-cv = pickle.load(open("model/cv.pkl","rb"))
-clf = pickle.load(open("model/clf.pkl","rb"))
+cv = pickle.load(open("model/cv.pkl","rb"))   # for converting the text emails into the numbers
+clf = pickle.load(open("model/clf.pkl","rb"))  # for predicting the Output of ML model
 
 @app.route("/")
 def home():
@@ -29,7 +31,7 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict():
     email = request.form.get('email-content')
-    tokenized_email = cv.transform([email]) # X 
+    tokenized_email = cv.transform([email]) 
     prediction = clf.predict(tokenized_email)
     prediction = "Spam" if prediction == 1 else "Not a Spam"
     return render_template("index.html", prediction=prediction, email=email)
